f9bb549f295522386d78beb58c6f9f2e0aacccf4
Wei-Chen Liu <nyy100604@gmail.com>
2024-07-06 00:56:58 +0800

    Modify file code in middleware

--- middleware.ts
+++ middleware.ts
@@ -78,4 +78,12 @@ export default auth((req) => {
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  runtime: "edge",
+  unstable_allowDynamic: [
+    "/mongoDb/schema/userSchema.ts",
+    "/mongoDb/schema/policySchema.ts",
+    "/mongoDb/connect/index.ts",
+    "/node_modules/mongoose/**",
+    "/lib/action/dataAction.ts",
+  ],
 };
